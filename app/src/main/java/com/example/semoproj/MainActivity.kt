9fc51abf2a645b4463976831bc0d3c5ce9486241
ksunbum97 <ksunbum97@gmail.com>
2022-11-16 14:04:16 +0900

    initial project

--- app/src/main/java/com/example/semoproj/MainActivity.kt
+++ app/src/main/java/com/example/semoproj/MainActivity.kt
@@ -2,13 +2,64 @@ package com.example.semoproj
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.semoproj.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var toggle: ActionBarDrawerToggle
+
+    class MyFragmentPagerAdapter(activity: FragmentActivity): FragmentStateAdapter(activity){
+        val fragments: List<Fragment>
+        init {
+            fragments= listOf(OneFragment(), TwoFragment(), ThreeFragment())
+        }
+        override fun getItemCount(): Int = fragments.size
+
+        override fun createFragment(position: Int): Fragment = fragments[position]
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setSupportActionBar(binding.toolbar)
+        toggle = ActionBarDrawerToggle(this, binding.drawer, R.string.drawer_opened,
+            R.string.drawer_closed)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toggle.syncState()
+        // 뷰 페이지에 어댑터 적용
+        val adapter = MyFragmentPagerAdapter(this)
+        binding.viewpager.adapter = adapter
+        TabLayoutMediator(binding.tabs, binding.viewpager) { tab, position ->
+            tab.text = "Tab${position +1}"
+        }.attach()
+
+        binding.fab.setOnClickListener {
+            when(binding.fab.isExtended) {
+                true -> binding.fab.shrink()
+                false -> binding.fab.extend()
+            }
+        }
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
