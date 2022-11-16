a9e5c1e3fed6b30a2ffdd538b769c1e791c6c2a3
ksunbum97 <ksunbum97@gmail.com>
2022-11-16 15:26:35 +0900

    no drawer

--- app/src/main/java/com/example/semoproj/MainActivity.kt
+++ app/src/main/java/com/example/semoproj/MainActivity.kt
@@ -44,7 +44,6 @@ class MainActivity : AppCompatActivity() {
                 false -> binding.fab.extend()
             }
         }
-
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
