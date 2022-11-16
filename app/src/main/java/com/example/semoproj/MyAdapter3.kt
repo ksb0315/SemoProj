e195fb3b43c166977e1c7efe42f4c1529e7675e0
B-JayU <qudwn8712@naver.com>
2022-11-16 19:26:21 +0900

    19시 26분 커밋

---
+++ app/src/main/java/com/example/semoproj/MyAdapter3.kt
@@ -0,0 +1,50 @@
+package com.example.semoproj
+
+import android.content.Context
+import android.graphics.Color
+import android.graphics.Rect
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.ViewCompat
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.semoproj.databinding.ItemRecyclerviewBinding
+import com.example.semoproj.databinding.ItemRecyclerviewSnapBinding
+
+class MyAdapter3(val datas: MutableList<MutableList<String>>): RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+
+    class MyViewHolder(val binding: ItemRecyclerviewSnapBinding): RecyclerView.ViewHolder(binding.root)
+
+    override fun getItemCount(): Int{
+        return datas.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder
+            = MyViewHolder(ItemRecyclerviewSnapBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val binding=(holder as MyViewHolder).binding
+        binding.whoRankSnap.text = "UserNumber의 Ranking Chart"
+        binding.RecyclerViewSnap.
+
+    }
+}
+
+class MyDecoration3(val context: Context): RecyclerView.ItemDecoration() {
+
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        super.getItemOffsets(outRect, view, parent, state)
+        val index = parent.getChildAdapterPosition(view) + 1
+
+        ViewCompat.setElevation(view, 20.0f)
+
+    }
+}
+
--- app/src/main/java/com/example/semoproj/ThreeFragment.kt
+++ app/src/main/java/com/example/semoproj/ThreeFragment.kt
@@ -1,10 +1,14 @@
 package com.example.semoproj
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.semoproj.databinding.FragmentThreeBinding
 
 
 class ThreeFragment : Fragment() {
@@ -15,8 +19,97 @@ class ThreeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_three, container, false)
+        val binding = FragmentThreeBinding.inflate(inflater, container, false)
+
+        val datas3 = mutableListOf<MutableList<String>>()
+
+        val name : List<String> = listOf("옛날포장마차",
+            "으뜸전산컴퓨터",
+            "박가냉면",
+            "종합꽃",
+            "장충동왕족발",
+            "NEO",
+            "한길부동산리서치",
+            "대성서점문구지도",
+            "김경숙쌤의티앤코수학학원",
+            "한남광고",
+            "노루페인트",
+            "건영공인중개사",
+            "달성천막공업사",
+            "굽네치킨",
+            "삼성상회",
+            "주원일렉콤",
+            "아가방앤컴퍼니수성동아NC점",
+            "씨에스베스트",
+            "슈에뜨",
+            "그린플라워꽃",
+            "닭S포차",
+            "비테속셈학원",
+            "태양코팅산업",
+            "통일천막공업사",
+            "미림식당",
+            "풍미식당",
+            "신정분식식당",
+            "무림객잔",
+            "마리",
+            "신헤어라인3미용실",
+            "삼천포건어물",
+            "고운맛식품",
+            "공원보리밥",
+            "이룸치과기공소",
+            "주도락",
+            "들바다푸드시스템",
+            "명가식품",
+            "리스본주점",
+            "둥지보신탕",
+            "미니카페",
+            "대하축산",
+            "코메루",
+            "휴대폰ACC",
+            "똘똘이동태전문식당",
+            "커피명가",
+            "김도영쌩얼미인",
+            "카카오pcCULB",
+            "이앤권헤어살롱",
+            "탑스쿨",
+            "스타벅스",
+            "이음도예공방",
+            "협신전기",
+            "미팜스튜디오",
+            "백운장여관",
+            "복권나라",
+            "상아마을",
+            "신토불이",
+            "서순자손수제비",
+            "애플",
+            "나이스식육",
+            "광원수산.",
+            "스랍음악학원",
+            "로엠동성로",
+            "정봉이닭발",
+            "카페나니아",
+            "나윤라이프",
+            "MR우드닥터",
+            "동경세탁",
+            "원진수퍼",
+            "루크안경"
+        )
+
+        for(i in 1..50){
+            val temp = mutableListOf<String>()
+            temp.add(i.toString())
+            temp.add(name[i-1])
+            datas3.add(temp)
+        }
+
+        val layoutManager = LinearLayoutManager(activity)
+        binding.recyclerViewSnapOuter.layoutManager=layoutManager
+        val adapter = MyAdapter3(datas3)
+        binding.recyclerViewSnapOuter.adapter=adapter
+        binding.recyclerViewSnapOuter.addItemDecoration(MyDecoration3(activity as Context))
+
+        binding.recyclerViewSnapOuter.addItemDecoration(DividerItemDecoration(context, LinearLayoutManager.VERTICAL))
+
+        return binding.root
     }
-
-
 }
