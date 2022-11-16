ec7d46b980b26fd6d40ab65bcc51ef3bd257e404
lacoon2874 <33238887+lacoon2874@users.noreply.github.com>
2022-11-16 17:22:41 +0900

    add divider

--- app/src/main/java/com/example/semoproj/OneFragment.kt
+++ app/src/main/java/com/example/semoproj/OneFragment.kt
@@ -6,8 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.semoproj.databinding.FragmentOneBinding
+import java.lang.Math.round
 
 
 class OneFragment : Fragment() {
@@ -25,7 +27,7 @@ class OneFragment : Fragment() {
             val temp = mutableListOf<String>()
             temp.add(i.toString())
             temp.add("player $i")
-            temp.add((5-i*0.02f).toString())
+            temp.add((round((5 - i * 0.02f) * 100) /100f).toString())
             datas.add(temp)
         }
 
@@ -34,6 +36,8 @@ class OneFragment : Fragment() {
         val adapter=MyAdapter(datas)
         binding.recyclerView.adapter=adapter
         binding.recyclerView.addItemDecoration(MyDecoration(activity as Context))
+
+        binding.recyclerView.addItemDecoration(DividerItemDecoration(context, LinearLayoutManager.VERTICAL))
 
         return binding.root
     }
